6f99c3a6c29dc8f02f2938b4e2106e6522b9425c
ObjoraDdd <trunova397@gmail.com>
2025-12-05 12:50:41 +0700

    fix login

--- backend/src/main/java/org/example/task_manager/TaskManagerApplication.java
+++ backend/src/main/java/org/example/task_manager/TaskManagerApplication.java
@@ -2,6 +2,7 @@ package org.example.task_manager;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 public class TaskManagerApplication {
